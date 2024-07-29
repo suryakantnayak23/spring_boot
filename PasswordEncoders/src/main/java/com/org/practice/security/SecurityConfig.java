f44dac6070b4d405ee441e1b57c79edba6cb97d6
Suryakant Nayak <suryakantnayak40@>
2024-07-29 13:38:56 +0530

    using bd storage for authentication details

--- PasswordEncoders/src/main/java/com/org/practice/security/SecurityConfig.java
+++ PasswordEncoders/src/main/java/com/org/practice/security/SecurityConfig.java
@@ -12,8 +12,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
  import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
 
 import static javax.management.Query.and;
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -30,8 +33,8 @@ public class SecurityConfig {
         return http.build();
     }
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user")
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+/*        UserDetails user = User.withUsername("user")
                 .password("{noop}12345")
                 .authorities("read")
                 .build();
@@ -39,7 +42,8 @@ public class SecurityConfig {
                 .password("{bcrypt}$2a$12$lJxaXXV8hz.xB9vI27rvDe3BSQgb71xMQI638mz/n2W.GwgA8h28.")
                 .authorities("admin")
                 .build();
-        return new InMemoryUserDetailsManager(user, admin);
+        return new InMemoryUserDetailsManager(user, admin);*/ //using inmemory storage
+        return new JdbcUserDetailsManager(dataSource);
     }
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -50,4 +54,6 @@ public class SecurityConfig {
     CompromisedPasswordChecker compromisedPasswordChecker() {
         return new HaveIBeenPwnedRestApiPasswordChecker();
     }
+
+
 }
